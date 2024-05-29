4a2fac356dcbc39549deb3e59f2e793383cdbfe3
robzard <pal-vitek1111@mail.ru>
2024-05-29 14:42:32 +0500

    service_account

--- google_sheets/gs_manager.py
+++ google_sheets/gs_manager.py
@@ -1,7 +1,7 @@
 import os.path
-
+from google.oauth2.service_account import Credentials
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+# from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -18,22 +18,25 @@ def get_wsh(wsh_name: str):
     Prints values from a sample spreadsheet.
     """
     creds = None
+
+    creds = Credentials.from_service_account_file('./google_sheets/service_account.json', scopes=SCOPES)
+
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("./google_sheets/token.json"):
-        creds = Credentials.from_authorized_user_file("./google_sheets/token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "./google_sheets/credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("./google_sheets/token.json", "w") as token:
-            token.write(creds.to_json())
+    # if os.path.exists("./google_sheets/token.json"):
+    #     creds = Credentials.from_authorized_user_file("./google_sheets/token.json", SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             "./google_sheets/credentials.json", SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open("./google_sheets/token.json", "w") as token:
+    #         token.write(creds.to_json())
 
     service = build("sheets", "v4", credentials=creds)
 
